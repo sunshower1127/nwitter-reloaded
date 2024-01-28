68cc37cc3889fb8f266f30f1a8ff5ec5caf13848
Sunwoo Kim <sunshower1127@gmail.com>
2024-01-28 10:15:19 +0900

    0128
    firebase auto 추가
    로그인 form 만들어봄

--- src/routes/create-account.tsx
+++ src/routes/create-account.tsx
@@ -1,3 +1,57 @@
+import { useState } from "react"
+import styled from "styled-components"
+
+const Wrapper = styled.div`
+
+`
+
+const Title = styled.h1`
+  
+`
+
+const Form = styled.form`
+  
+`
+
+const Input = styled.input`
+  
+`
+
 export default function CreateAccount() {
-  return <h1>create account</h1>
+  const [isLoading, setLoading] = useState(false)
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [error, setError] = useState("")
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { target: {name, value} } = e
+    if (name === "name") {
+      setName(value)
+    } else if (name === "password") {
+      setPassword(value)
+    } else if (name === "email") {
+      setEmail(value)
+    }
+  }
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    console.log(name, email, password);
+    try {
+
+    } catch(e) {
+
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  return <Wrapper>
+    <Title>Log into X</Title>
+    <Form onSubmit={onSubmit}>
+      <Input onChange={onChange} name="name" value={name} placeholder="Name" type="text" required />
+      <Input onChange={onChange} name="email" value={email} placeholder="Email" type="email" required />
+      <Input onChange={onChange} name="password" value={password} placeholder="Password" type="password" required />
+      <Input onChange={onChange} type="submit" value={isLoading ? "Loading" : "Create Account"} />
+    </Form>
+  </Wrapper>
 }
