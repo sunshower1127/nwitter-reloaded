cb9096c8949ac04f6bbd4d59bb9ca8b404cce7c2
Sunwoo Kim <sunshower1127@gmail.com>
2024-02-10 22:17:00 +0900

    navigation bar

--- src/components/github-btn.tsx
+++ src/components/github-btn.tsx
@@ -26,8 +26,14 @@ const Logo = styled.img`
 export default function GithubButton() {
   const navigate = useNavigate()
   const onClick = async() => {
-    
     try {
+      /* Github Auth 세팅
+       * 1. Firebase Auth에 새 제공업체 추가 -> Github
+       * 2. 클라이언트 ID, 보안 비밀번호를 입력해야하는데, 이건 github에서 생성해야함
+       * 3. github - Settings - Developer Settings - New OAuth Application
+       * 4. Application name, Homepage URL, Application description은 형식적인거라 대충 쓰면됨.
+       * 5. 콜백 링크만 firebase에서 복사해서 넣어주면 ID, 보안 비밀번호 나오는데 그거 firebase에 넣으면 끝.
+       */
       const provider = new GithubAuthProvider()
       await signInWithPopup(auth, provider)
       navigate('/')
--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,11 +1,3 @@
-import { useNavigate } from "react-router-dom"
-import { auth } from "../components/firebase"
-
 export default function Home() {
-  const navigate = useNavigate()
-  const logOut = async() => {
-    await auth.signOut()
-    navigate('/login')
-  }
-  return <h1><button onClick={logOut}>Log Out</button></h1>
+  return 'Home'
 }
