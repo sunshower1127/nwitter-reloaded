a4fe4afd041fefd13fed2c1889ad27bd3ab1a96c
Sunwoo Kim <sunshower1127@gmail.com>
2024-02-13 11:59:43 +0900

    file attachment

--- src/components/post-tweet-form.tsx
+++ src/components/post-tweet-form.tsx
@@ -1,7 +1,8 @@
-import { addDoc, collection } from "firebase/firestore"
+import { addDoc, collection, updateDoc } from "firebase/firestore"
 import { useState } from "react"
 import styled from "styled-components"
-import { auth, db } from "./firebase"
+import { auth, db, storage } from "./firebase"
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage"
 
 const Form = styled.form`
   display: flex;
@@ -81,15 +82,23 @@ export default function PostTweetForm() {
   const onSubmit = async(e: React.ChangeEvent<HTMLFormElement>) => {
     e.preventDefault()
     const user = auth.currentUser
-    if (!user || isLoading || tweet == "" || tweet.length > 180) return
+    if (!user || isLoading || tweet.trim() == "" || tweet.length > 180) return
     try {
       setLoading(true)
-      await addDoc(collection(db, "tweets"), {
+      const doc = await addDoc(collection(db, "tweets"), {
         tweet,
         createdAt: Date.now(),
         username: user.displayName || "Anonymous",
         userId: user.uid
       })
+      if (file) {
+        const locationRef = ref(storage, `tweets/${user.uid}-${user.displayName}/${doc.id}`)
+        const result = await uploadBytes(locationRef, file)
+        const url = await getDownloadURL(result.ref)
+        await updateDoc(doc, { photo: url })
+      }
+      setTweet('')
+      setFile(null)
     } catch(e) {
       console.log(e);
     } finally {
@@ -104,6 +113,7 @@ export default function PostTweetForm() {
         placeholder="Type right here"
         rows={5}
         maxLength={180}
+        required
       ></TextArea>
       <AttachFileButton htmlFor="file-submit">
         {file? "Photo added V" : "Add photo"}
